66034c9d59202ae5458ad0d54f8a9487be7e53af
^_^ <8480595+ballaballaballa@users.noreply.github.com>
2026-03-09 01:23:33 +0100

    Updated PODME_API_USER_AGENT as previous stopped working (#12)

--- podme_api/const.py
+++ podme_api/const.py
@@ -5,7 +5,7 @@ import logging
 from podme_api.models import PodMeRegion
 
 PODME_AUTH_USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0"
-PODME_API_USER_AGENT = "Podme android app/6.29.3 (Linux;Android 15) AndroidXMedia3/1.5.1"
+PODME_API_USER_AGENT = "Podme android app/6.38.5 (Linux;Android 15) AndroidXMedia3/1.5.1"
 PODME_API_BASE_URL = "https://api.podme.com"
 PODME_BASE_URL = "https://podme.com"
 
